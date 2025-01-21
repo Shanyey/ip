604b3c49238f92c48a1520b39d904584c238da62
Shanyey <sleongxy@gmail.com>
2025-01-21 22:12:02 +0800

    Edit variable name

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -1,8 +1,8 @@
 public class Nova {
     public static void main(String[] args) {
-        String greeting = "Hello! I'm Nova" + '\n' + "What can I do fpr you?";
-        String closing = "Bye. Hope to see you again soon!";
-        System.out.println(greeting);
-        System.out.println(closing);
+        String greet = "Hello! I'm Nova" + '\n' + "What can I do fpr you?";
+        String exit = "Bye. Hope to see you again soon!";
+        System.out.println(greet);
+        System.out.println(exit);
     }
 }
