5acd2749e4ee972197c5be9d0939408ae4f1e38a
Shanyey <sleongxy@gmail.com>
2025-01-22 00:33:12 +0800

    Echo

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class Nova {
     public static void main(String[] args) {
-        String greet = "Hello! I'm Nova" + '\n' + "What can I do fpr you?";
+        String greet = "Hello! I'm Nova" + '\n' + "What can I do for you?";
         String exit = "Bye. Hope to see you again soon!";
         System.out.println(greet);
-        System.out.println(exit);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(exit);
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+
     }
 }
