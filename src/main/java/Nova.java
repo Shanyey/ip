4f23fad5eccd742c7f78599562e07afc023b24d8
Shanyey <sleongxy@gmail.com>
2025-01-21 22:10:00 +0800

    level-0: rename, Greet, Exit

---
+++ src/main/java/Nova.java
@@ -0,0 +1,8 @@
+public class Nova {
+    public static void main(String[] args) {
+        String greeting = "Hello! I'm Nova" + '\n' + "What can I do fpr you?";
+        String closing = "Bye. Hope to see you again soon!";
+        System.out.println(greeting);
+        System.out.println(closing);
+    }
+}
