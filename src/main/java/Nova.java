c6dd560c9857278bfdf24db8e1eace4c1b1bb272
Shanyey <sleongxy@gmail.com>
2025-01-23 00:21:58 +0800

    Halfway done with Deadline class

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -6,6 +6,7 @@ public class Nova {
         //initialise variables
         ArrayList<Task> array = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        String addedToList = "Got it. I've added this task:";
 
         System.out.println("Hello! I'm Nova" + '\n' + "What can I do for you?");
         while (true) {
@@ -23,15 +24,28 @@ public class Nova {
                 }
             } else { //check action
                 String[] words = input.split(" ");
-                if (words[0].equals("mark")) { // check for mark
-                    array.get(Integer.parseInt(words[1]) - 1).done();
-                    System.out.println("Nice! I've marked this task as done:" + '\n' + array.get(Integer.parseInt(words[1]) - 1));
-                } else if (words[0].equals("unmark")) {// check for unmark
-                    array.get(Integer.parseInt(words[1]) - 1).undone();
-                    System.out.println("OK, I've marked this task as not done yet:" + '\n' + array.get(Integer.parseInt(words[1]) - 1));
-                } else {//add task to list
-                    array.add(new Task(input));
-                    System.out.println("added: " + input);
+                switch (words[0]) {
+                    case "mark" -> {
+                        array.get(Integer.parseInt(words[1]) - 1).done();
+                        System.out.println("Nice! I've marked this task as done:" + '\n' + array.get(Integer.parseInt(words[1]) - 1));
+                    }
+                    case "unmark" -> {
+                        array.get(Integer.parseInt(words[1]) - 1).undone();
+                        System.out.println("OK, I've marked this task as not done yet:" + '\n' + array.get(Integer.parseInt(words[1]) - 1));
+                    }
+                    case "deadline" -> {
+                        String[] words2 = input.split("/");
+                        String deadline = words2[1];
+                        String task = words2[0].replace("deadline", "");
+                        Deadline activity = new Deadline(task, deadline);
+                        array.add(activity);
+                        System.out.println(addedToList + '\n' + activity);
+                    }
+                    default -> {
+                        Task task = new Task(input);
+                        array.add(task);
+                        System.out.println(addedToList + '\n' + task);
+                    }
                 }
             }
         }
