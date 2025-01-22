471b481b6e49ae8578264c1817134a78b16600b3
Shanyey <sleongxy@gmail.com>
2025-01-22 18:38:50 +0800

    Level 3: mark as done

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -1,26 +1,38 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Nova {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Nova" + '\n' + "What can I do for you?");
-
+        //initialise variables
+        ArrayList<Task> array = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        String[] list = new String[100];
-        int i = 0;
+
+        System.out.println("Hello! I'm Nova" + '\n' + "What can I do for you?");
         while (true) {
             String input = scanner.nextLine();
+            input = input.toLowerCase();
+
             /* exit out of chat */
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if (input.equals("list")) {
-                for (int j = 0; j < i; j++) {
-                    System.out.println(j+1 + ". " + list[j]);
+            } else if (input.equals("list")) { /* print out all tasks*/
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < array.size(); i++) {
+                    System.out.println((i+1) + "." + array.get(i));
                 }
-            } else {
-                list[i] = input;
-                i += 1;
-                System.out.println("added: "+ input);
+            } else { //check action
+                String[] words = input.split(" ");
+                if (words[0].equals("mark")) { // check for mark
+                    array.get(Integer.parseInt(words[1]) - 1).done();
+                    System.out.println("Nice! I've marked this task as done:" + '\n' + array.get(Integer.parseInt(words[1]) - 1));
+                } else if (words[0].equals("unmark")) {// check for unmark
+                    array.get(Integer.parseInt(words[1]) - 1).undone();
+                    System.out.println("OK, I've marked this task as not done yet:" + '\n' + array.get(Integer.parseInt(words[1]) - 1));
+                } else {//add task to list
+                    array.add(new Task(input));
+                    System.out.println("added: " + input);
+                }
             }
         }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,20 @@
+public class Task {
+    private final String description;
+    private boolean isDone = false; //set to false
+
+    public Task(String description) {
+        this.description = description;
+    }
+    public void done() {
+        isDone = true;
+    }
+
+    public void undone() {
+        isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return (isDone? "[X] " : "[ ] ") + description;
+    }
+}
