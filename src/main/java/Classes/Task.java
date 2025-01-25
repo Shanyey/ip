2cd996b8501e80dbd5d47aa5bad13177825be520
Shanyey <sleongxy@gmail.com>
2025-01-25 15:35:19 +0800

    optimise code add error testing for mark, unmark

--- src/main/java/Classes/Task.java
+++ src/main/java/Classes/Task.java
@@ -7,12 +7,17 @@ public class Task {
     public Task(String description) {
         this.description = description;
     }
+
     public void setDone() {
         isDone = true;
     }
 
     public void setNotDone() {
         isDone = false;
+    }
+
+    public boolean isDone() {
+        return isDone;
     }
 
     @Override
--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -7,73 +7,108 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Nova {
-    private static void printMark(Task task) {
-        System.out.println("Wow you actually managed to finish this task");
-        System.out.println(task);
-    }
-    private static void printUnMark(Task task) {
-        System.out.println("I knew you wouldn't finish this task");
-        System.out.println(task);
-    }
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final ArrayList<Task> tasks = new ArrayList<>();
+
+
     private static void printAddTask(Task task, int tasksSize) {
         System.out.println("I bet you're gonna be to lazy to do it anyway *rolls eyes*" + "\n" + task);
         System.out.println("Now you have " + tasksSize + " tasks in the list.");
     }
 
-    public static void main(String[] args) {
-        //initialise variables
-        ArrayList<Task> tasks = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
+    //handles invalid 1 word inputs
+    private static String[] parseAction(String action) throws NovaException {
+        action = action.trim();
+        String[] splitAction = action.split(" ", 2);
+        if (splitAction.length == 1) {
+            throw new NovaException("too little arguments or invalid command");
+        }
+        return splitAction;
+    }
 
+    private static void executeMark(int index) throws NovaException {
+        if (index > tasks.size() || index <= 0) {
+            throw new NovaException("invalid task number");
+        }
+        Task task = tasks.get(index - 1);
+        if (task.isDone()) {
+            throw new NovaException("task is already done");
+        } else {
+            task.setDone();
+            System.out.println("Wow you actually managed to finish this task" + "\n" + task);
+        }
+    }
+
+    private static void executeUnMark(int index) throws NovaException {
+        if (index > tasks.size() || index <= 0) {
+            throw new NovaException("invalid task number");
+        }
+        Task task = tasks.get(index - 1);
+        if (!task.isDone()) {
+            throw new NovaException("task is already unmarked");
+        } else {
+            task.setNotDone();
+            System.out.println("I knew you wouldn't finish this task" + "\n" + task);
+        }
+    }
+
+    public static void main(String[] args) {
         //start loop and get inputs
         System.out.println("Ughh, what can i do for you?");
         while (true) {
             String action = scanner.nextLine();
-            if (action.equals("Bye")) {
+            if (action.equalsIgnoreCase("bye")) {
                 System.out.println("Goodbye! Hope to see you never!");
                 break;
-            } else if (action.equals("List")) {
+            } else if (action.equalsIgnoreCase("list")) {
                 System.out.println("These are the tasks:");
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + "." + tasks.get(i));
+                if (tasks.isEmpty()) {
+                    System.out.println("No tasks added.");
+                } else {
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + "." + tasks.get(i));
+                    }
                 }
             } else {
-                String[] splitAction = action.split(" ", 2);
-                String[] slashedAction = splitAction[1].split("/");
-                String description = slashedAction[0].trim();
+                try {
+                    String[] splitAction = parseAction(action);
+                    String[] slashedAction = splitAction[1].split("/");
+                    String description = slashedAction[0].trim();
 
-                switch (splitAction[0].toLowerCase()) {
-                    case "mark" -> {
-                        Task task = tasks.get(Integer.parseInt(splitAction[1]) - 1);
-                        task.setDone();
-                        printMark(task);
+                    switch (splitAction[0].toLowerCase()) {
+                        case "mark" -> {
+                            executeMark(Integer.parseInt(splitAction[1]));
+                        }
+                        case "unmark" -> {
+                            executeUnMark(Integer.parseInt(splitAction[1]));
+                        }
+                        case "todo" -> {
+                            //replace with executeToDo
+                            Todo todo = new Todo(splitAction[1]);
+                            tasks.add(todo);
+                            printAddTask(todo, tasks.size());
+                        }
+                        case "deadline" -> {
+                            //replace with execute deadline
+                            String date = slashedAction[1].replace("by ", "");
+                            Deadline deadline = new Deadline(description, date);
+                            tasks.add(deadline);
+                            printAddTask(deadline, tasks.size());
+                        }
+                        case "event" -> {
+                            //replace with execute event
+                            String from = slashedAction[1].replace("from ", "").trim();
+                            String to = slashedAction[2].replace("to ", "");
+                            Event event = new Event(description, from, to);
+                            tasks.add(event);
+                            printAddTask(event, tasks.size());
+                        }
+                        default -> {
+                            System.out.println("Fam I don't know what you are yapping about");
+                        }
                     }
-                    case "unmark" -> {
-                        Task task = tasks.get(Integer.parseInt(splitAction[1]) - 1);
-                        task.setNotDone();
-                        printUnMark(task);
-                    }
-                    case "todo" -> {
-                        Todo todo = new Todo(splitAction[1]);
-                        tasks.add(todo);
-                        printAddTask(todo, tasks.size());
-                    }
-                    case "deadline" -> {
-                        String date = slashedAction[1].replace("by ", "");
-                        Deadline deadline = new Deadline(description, date);
-                        tasks.add(deadline);
-                        printAddTask(deadline, tasks.size());
-                    }
-                    case "event" -> {
-                        String from = slashedAction[1].replace("from ", "").trim();
-                        String to = slashedAction[2].replace("to ", "");
-                        Event event = new Event(description, from, to);
-                        tasks.add(event);
-                        printAddTask(event, tasks.size());
-                    }
-                    default -> {
-                        System.out.println("Fam I don't know what you are yapping about");
-                    }
+                } catch (NovaException e) {
+                    System.out.println(e.getMessage());
                 }
             }
         }
