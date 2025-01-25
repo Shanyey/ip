a71979733f7841a41035651383013dda305a212e
Shanyey <sleongxy@gmail.com>
2025-01-26 00:22:06 +0800

    add error test

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -10,13 +10,13 @@ public class Nova {
     private static final Scanner scanner = new Scanner(System.in);
     private static final ArrayList<Task> tasks = new ArrayList<>();
 
-
+    //print string after adding a task
     private static void printAddTask(Task task, int tasksSize) {
         System.out.println("I bet you're gonna be to lazy to do it anyway *rolls eyes*" + "\n" + task);
         System.out.println("Now you have " + tasksSize + " tasks in the list.");
     }
 
-    //handles invalid 1 word inputs
+    //parses input and handles invalid 1 word inputs
     private static String[] parseAction(String action) throws NovaException {
         action = action.trim();
         String[] splitAction = action.split(" ", 2);
@@ -26,6 +26,7 @@ public class Nova {
         return splitAction;
     }
 
+    //marks tasks as done
     private static void executeMark(int index) throws NovaException {
         if (index > tasks.size() || index <= 0) {
             throw new NovaException("invalid task number");
@@ -39,6 +40,7 @@ public class Nova {
         }
     }
 
+    //marks task as not done
     private static void executeUnMark(int index) throws NovaException {
         if (index > tasks.size() || index <= 0) {
             throw new NovaException("invalid task number");
@@ -50,6 +52,43 @@ public class Nova {
             task.setNotDone();
             System.out.println("I knew you wouldn't finish this task" + "\n" + task);
         }
+    }
+
+    //add To-do task to the list
+    private static void addToDo(String desc) {
+        Todo todo = new Todo(desc);
+        tasks.add(todo);
+        printAddTask(todo, tasks.size());
+    }
+
+    //add Deadline tasks to the list
+    private static void addDeadline(String[] slashedAction) throws NovaException {
+        if (slashedAction.length < 2) {
+            throw new NovaException("too little arguments");
+        }
+        String desc = slashedAction[0].trim();
+        String deadline = slashedAction[1].trim();
+        if (desc.isEmpty() || !deadline.contains("by ")) {
+            throw new NovaException("invalid format");
+        }
+        Deadline task = new Deadline(desc, deadline.replace("by ", ""));
+        tasks.add(task);
+        printAddTask(task, tasks.size());
+    }
+
+    private static void addEvent(String[] slashedAction) throws NovaException {
+        if (slashedAction.length < 3) {
+            throw new NovaException("too little arguments");
+        }
+        String desc = slashedAction[0].trim();
+        String from = slashedAction[1].trim();
+        String to = slashedAction[2].trim();
+        if (desc.isEmpty() || !from.contains("from ") || !to.contains("to ")) {
+            throw new NovaException("invalid format");
+        }
+        Event event = new Event(desc, from.replace("from ", ""), to.replace("to ", ""));
+        tasks.add(event);
+        printAddTask(event, tasks.size());
     }
 
     public static void main(String[] args) {
@@ -73,39 +112,13 @@ public class Nova {
                 try {
                     String[] splitAction = parseAction(action);
                     String[] slashedAction = splitAction[1].split("/");
-                    String description = slashedAction[0].trim();
-
                     switch (splitAction[0].toLowerCase()) {
-                        case "mark" -> {
-                            executeMark(Integer.parseInt(splitAction[1]));
-                        }
-                        case "unmark" -> {
-                            executeUnMark(Integer.parseInt(splitAction[1]));
-                        }
-                        case "todo" -> {
-                            //replace with executeToDo
-                            Todo todo = new Todo(splitAction[1]);
-                            tasks.add(todo);
-                            printAddTask(todo, tasks.size());
-                        }
-                        case "deadline" -> {
-                            //replace with execute deadline
-                            String date = slashedAction[1].replace("by ", "");
-                            Deadline deadline = new Deadline(description, date);
-                            tasks.add(deadline);
-                            printAddTask(deadline, tasks.size());
-                        }
-                        case "event" -> {
-                            //replace with execute event
-                            String from = slashedAction[1].replace("from ", "").trim();
-                            String to = slashedAction[2].replace("to ", "");
-                            Event event = new Event(description, from, to);
-                            tasks.add(event);
-                            printAddTask(event, tasks.size());
-                        }
-                        default -> {
-                            System.out.println("Fam I don't know what you are yapping about");
-                        }
+                        case "mark" -> executeMark(Integer.parseInt(splitAction[1]));
+                        case "unmark" -> executeUnMark(Integer.parseInt(splitAction[1]));
+                        case "todo" -> addToDo(splitAction[1]);
+                        case "deadline" -> addDeadline(slashedAction);
+                        case "event" -> addEvent(slashedAction);
+                        default -> System.out.println("Fam I don't know what you are yapping about");
                     }
                 } catch (NovaException e) {
                     System.out.println(e.getMessage());
