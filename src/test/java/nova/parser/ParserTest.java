fc311cfcf67f0999a896f2107087581bd338a4c5
Shanyey <sleongxy@gmail.com>
2025-02-05 15:06:39 +0800

    add JUNIT test

---
+++ src/test/java/nova/parser/ParserTest.java
@@ -0,0 +1,35 @@
+package nova.parser;
+
+import nova.exceptions.NovaException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class ParserTest {
+
+    @Test
+    public void parseBySpace_exceptionThrown() {
+        try {
+            Parser p = new Parser();
+            String invalidInput = "todo";
+            p.parseBySpace(invalidInput);
+        } catch (NovaException e) {
+            assertEquals("too little arguments or invalid command", e.getMessage());
+        }
+    }
+
+    @Test
+    public void parseBySpace_success() {
+        Parser p = new Parser();
+        String[] words;
+        try {
+            String correctInput = "todo read book";
+            words = p.parseBySpace(correctInput);
+            assertEquals("todo", words[0]);
+            assertEquals("read book", words[1]);
+        } catch (NovaException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
---
+++ src/test/java/nova/taskList/TaskListTest.java
@@ -0,0 +1,43 @@
+package nova.taskList;
+
+import nova.classes.Task;
+import nova.exceptions.NovaException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class TaskListTest {
+    private final String[] arr = {"lecture", "from 2019-01-01 19:30"};
+    private final String[] arrMissingFrom = {"lecture", "2019-01-01 19:30", "to 2019-01-01 20:30"};
+
+    @Test
+    public void addTodo_success() {
+        ArrayList<Task> tasks = new ArrayList<>();
+        TaskList tasksList = new TaskList(tasks);
+        tasksList.addToDo("read book");
+        assertEquals(1, tasks.size());
+    }
+
+    @Test
+    public void addEvent_exceptionThrown() {
+        ArrayList<Task> tasks = new ArrayList<>();
+        TaskList tasksList = new TaskList(tasks);
+        try {
+            tasksList.addEvent(arr);
+        } catch (NovaException e) {
+            assertEquals("too little arguments", e.getMessage());
+        }
+    }
+
+    @Test
+    public void addEvent_missingFrom_exceptionThrown() {
+        ArrayList<Task> tasks = new ArrayList<>();
+        TaskList tasksList = new TaskList(tasks);
+        try {
+            tasksList.addEvent(arrMissingFrom);
+        } catch (NovaException e) {
+            assertEquals("invalid format", e.getMessage());
+        }
+    }
+}
